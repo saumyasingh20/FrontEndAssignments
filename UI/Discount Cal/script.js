b0c43d0a7a566153fbb86f8bb038734e20dec6fd
saumyasingh20 <saumyaaaa98@gmail.com>
2022-07-12 15:53:26 +0530

    day2 assignment1

---
+++ UI/Discount Cal/script.js
@@ -0,0 +1,17 @@
+function calculateDiscount(){
+    amt = parseInt(document.getElementById("amt").value);
+    discPercent = 0
+    if(amt<=10000){
+        discPercent = 10;
+    }else if(amt>10000 && amt<=20000){
+        discPercent = 20;
+    }else {
+        discPercent = 25;
+    }
+    discount = (discPercent/100)*amt;
+    document.getElementById("disc").value = discount;
+    netpay = amt-discount;
+    document.getElementById("pay").value = netpay;
+    document.getElementById("savings").value = discPercent;
+
+}
